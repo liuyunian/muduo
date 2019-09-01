11f14808f9452088f6ca1c74d5369a3c17bd4447
liuyunian <sdjn_lyn@163.com>
2019-09-01 19:00:55 +0800

    annotate: Singleton

--- muduo/base/Singleton.h
+++ muduo/base/Singleton.h
@@ -17,10 +17,10 @@ namespace muduo
 
 namespace detail
 {
-// This doesn't detect inherited member functions!
+// This doesn't detect inherited member functions!                                              -- 这不会检测继承的成员函数
 // http://stackoverflow.com/questions/1966362/sfinae-to-check-for-inherited-member-functions
 template<typename T>
-struct has_no_destroy
+struct has_no_destroy                                                                           // 使用SFINAE检测是否有特定的成员函数
 {
   template <typename C> static char test(decltype(&C::no_destroy));
   template <typename C> static int32_t test(...);
@@ -28,16 +28,16 @@ struct has_no_destroy
 };
 }  // namespace detail
 
-template<typename T>
-class Singleton : noncopyable
+template<typename T>                                                                             // 模板类
+class Singleton : noncopyable                                                                    // 用于其他的类改造成单例类，返回传入参数
 {
  public:
-  Singleton() = delete;
-  ~Singleton() = delete;
+  Singleton() = delete;                                                                         // 禁用构造函数
+  ~Singleton() = delete;                                                                        // 禁用析构函数
 
   static T& instance()
   {
-    pthread_once(&ponce_, &Singleton::init);
+    pthread_once(&ponce_, &Singleton::init);                                                    // Singleto::init函数只被执行一次
     assert(value_ != NULL);
     return *value_;
   }
@@ -45,16 +45,21 @@ class Singleton : noncopyable
  private:
   static void init()
   {
-    value_ = new T();
+    value_ = new T();                                                                           // 创建T实例，该实例是唯一一个实例
     if (!detail::has_no_destroy<T>::value)
     {
-      ::atexit(destroy);
+      ::atexit(destroy);                                                                        // 使用atexit()函数注册进程退出时要执行的函数 -- destory
     }
   }
 
   static void destroy()
   {
-    typedef char T_must_be_complete_type[sizeof(T) == 0 ? -1 : 1];
+    typedef char T_must_be_complete_type[sizeof(T) == 0 ? -1 : 1];                              // 限制T必须是完成的类型
+                                                                                                /**
+                                                                                                 * class A;
+                                                                                                 * A * p_a = nullptr;
+                                                                                                 * 上面也是可以编译通过的，但是类A不是完整的类型，对于这样的类，不能作为Singleton模板参数
+                                                                                                */
     T_must_be_complete_type dummy; (void) dummy;
 
     delete value_;
@@ -62,12 +67,12 @@ class Singleton : noncopyable
   }
 
  private:
-  static pthread_once_t ponce_;
-  static T*             value_;
+  static pthread_once_t ponce_;                                                                 // pthread_once参数
+  static T*             value_;                                                                 // 指向T唯一的实例
 };
 
 template<typename T>
-pthread_once_t Singleton<T>::ponce_ = PTHREAD_ONCE_INIT;
+pthread_once_t Singleton<T>::ponce_ = PTHREAD_ONCE_INIT;                                        // 初始化为PTHREAD_ONCE_INIT，保证了在多线程环境下只被执行一次
 
 template<typename T>
 T* Singleton<T>::value_ = NULL;
